fc9821ba822c8e36e65b9b59b97e5897a35cf3f9
ola <raymond.oyinlola@gmail.com>
2022-07-21 07:50:25 +0800

    update kafka service

--- src/infrastructure/kafka/kafka-service.ts
+++ src/infrastructure/kafka/kafka-service.ts
@@ -2,9 +2,9 @@ import { ConfigService } from "@nestjs/config";
 import { Transport } from "@nestjs/microservices";
 
 export class KafkaService {
-  static getOptions(groupId: string, broker: string) {
+  static getOptions(groupId: string, broker: string, transport: number) {
     return {
-      transport: Transport.KAFKA,
+      transport,
       options: {
         client: {
           brokers: [broker],
